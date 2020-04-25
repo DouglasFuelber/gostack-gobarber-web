087ed008f7993be42ca7d13bca34fb387a3148ce
Douglas Fuelber <contato@douglasfuelber.com>
2020-04-25 17:05:08 -0300

    Created tooltip component

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -25,7 +25,7 @@ const SignUp: React.FC = () => {
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'Senha deve ter no mínimo 6 digitos'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
       });
 
       await schema.validate(data, {
