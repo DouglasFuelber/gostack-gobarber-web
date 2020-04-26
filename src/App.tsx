10476deed1223124a2c784ba29bd50992895b158
Douglas Fuelber <contato@douglasfuelber.com>
2020-04-26 20:09:16 -0300

    Created toast structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 
+import ToastContainer from './components/ToastContainer';
+
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
+      <ToastContainer />
     </AuthProvider>
     <GlobalStyle />
   </>
