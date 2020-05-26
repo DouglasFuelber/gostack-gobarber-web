ecc43de1b87baa98b5914895cb8aa2cd67df92d6
Douglas Fuelber <contato@douglasfuelber.com>
2020-05-25 22:43:12 -0300

    Next appointment layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/15067098?s=460&u=937ff779f5b702ee7cfc32da353fa97de9ecf252&v=4"
+                alt="Douglas Fuelber"
+              />
+              <strong>Douglas Fuelber</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -56,3 +56,95 @@ export const Profile = styled.div`
     }
   }
 `;
+
+export const Content = styled.main`
+  display: flex;
+  margin: 64px auto;
+  max-width: 1120px;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    align-items: center;
+    color: #ff9000;
+    display: flex;
+    margin-top: 8px;
+
+    span {
+      align-items: center;
+      display: flex;
+    }
+
+    span + span::before {
+      background: #ff9000;
+      content: '';
+      height: 12px;
+      margin: 0 8px;
+      width: 1px;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    align-items: center;
+    background: #3e3b47;
+    border-radius: 10px;
+    display: flex;
+    margin-top: 24px;
+    padding: 16px 24px;
+    position: relative;
+
+    &::before {
+      background: #ff9000;
+      content: '';
+      height: 80%;
+      left: 0;
+      position: absolute;
+      top: 10%;
+      width: 2px;
+    }
+
+    img {
+      border-radius: 50%;
+      height: 80px;
+      width: 80px;
+    }
+
+    strong {
+      color: #fff;
+      margin-left: 24px;
+    }
+
+    span {
+      align-items: center;
+      color: #999591;
+      display: flex;
+      margin-left: auto;
+
+      svg {
+        color: #ff9000;
+        margin-right: 8px;
+      }
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
+`;
